599c7e71119b9a4db6768701868471c35e431362
ChinnuButterfly <124045115+ChinnuButterfly@users.noreply.github.com>
2023-05-08 13:20:38 +0530

    Update java_installation.sh

--- java_installation.sh
+++ java_installation.sh
@@ -1,14 +1,11 @@
 #! /bin/bash 
-sudo apt update
-sudo su
-echo "Update completed!!!!"
 wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
 dpkg -i jdk-17_linux-x64_bin.deb
 export JAVA_HOME=/usr/lib/jvm/jdk-17/
 export PATH=$PATH:$JAVA_HOME/bin
 chmod 777 /etc/environment
-nano /etc/environment
 JAVA_HOME="/usr/lib/jvm/jdk-17"
+echo -e "$JAVA_HOME" >> /etc/environment
 source /etc/environment
 echo $JAVA_HOME
 echo "Java has been installed successfully"
