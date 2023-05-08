cc157276c07f54ed9cd46afe5f1b2350837b98a6
ChinnuButterfly <124045115+ChinnuButterfly@users.noreply.github.com>
2023-05-08 13:01:52 +0530

    Update java_installation.sh

--- java_installation.sh
+++ java_installation.sh
@@ -5,6 +5,7 @@ wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
 dpkg -i jdk-17_linux-x64_bin.deb
 export JAVA_HOME=/usr/lib/jvm/jdk-17/
 export PATH=$PATH:$JAVA_HOME/bin
+sudo chmod 777 /etc/environment
 nano /etc/environment
 JAVA_HOME="/usr/lib/jvm/jdk-17"
 source /etc/environment
